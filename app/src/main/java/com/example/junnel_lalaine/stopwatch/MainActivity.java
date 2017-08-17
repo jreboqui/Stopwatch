e62a0aa786712691805ab0a78ab58cd7a5088c22
Junnel Reboquio <jreboqui@ucsd.edu>
2017-08-16 21:34:09 -0700

    changed onStop and onStart to onPause and onResume

--- app/src/main/java/com/example/junnel_lalaine/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/junnel_lalaine/stopwatch/MainActivity.java
@@ -33,15 +33,15 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onStop() {
-        super.onStop();
+    protected void onPause() {
+        super.onPause();
         wasRunning = running;
         running = false;
     }
 
     @Override
-    protected void onStart() {
-        super.onStart();
+    protected void onResume() {
+        super.onResume();
         if (wasRunning) {
             running = true; //resumes the timer
         }
